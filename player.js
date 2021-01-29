a95a18b6aad0544c2b75c7515dd7fbef5aeb3972
Vikash Kumar <vikash.kumar@thoughtworks.com>
2021-01-29 20:39:00 +0530

    Added code to prevent miltiple script tag addition

--- player.js
+++ player.js
@@ -1,16 +1,20 @@
 jqScriptTag = document.createElement("script");
 jqScriptTag.type = "text/javascript";
+jqScriptTag.id = "jqScript";
 jqScriptTag.src =
   "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
 
-document.getElementsByTagName("head")[0].append(jqScriptTag);
+if (!document.getElementById("jqScript"))
+  document.getElementsByTagName("head")[0].append(jqScriptTag);
 
 playerScriptTag = document.createElement("script");
 playerScriptTag.type = "text/javascript";
+playerScriptTag.id = "playerScript";
 playerScriptTag.src =
   "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867";
 
-document.getElementsByTagName("head")[0].append(playerScriptTag);
+if (!document.getElementById("jqScript"))
+  document.getElementsByTagName("head")[0].append(playerScriptTag);
 
 var prevStepId;
 function __5szm2kaj(response) {
@@ -104,8 +108,8 @@ function modifyElementCss() {
   });
 
   $("button[data-iridize-role='prevBt']").css({
-      "max-width": "85px",
-      "margin-right": "10px"
+    "max-width": "85px",
+    "margin-right": "10px",
   });
 
   $("button[aria-label='Close']").css({
